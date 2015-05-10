7ab6913626806126fd8efd5284706edfc26cf5c7
Tikhon Belousko <dazmail01@gmail.com>
2015-05-10 19:19:38 +0300

    lab5: Сделал задание 1.

--- labs/lab5/task-1.s
+++ labs/lab5/task-1.s
@@ -5,16 +5,71 @@ _EXIT   = 1
         SHR     BX, 1
 
         PUSH    arr
+        PUSH    8
+        CALL    FIBONACCI
+        ADD     SP, 4
+
+        PUSH    arr
         PUSH    BX
         CALL    PRINT_ARR
+        ADD     SP, 4
 
         ! Exit program
         PUSH    0
         PUSH    _EXIT
         SYS
 
+FIBONACCI:
+        PUSH    BP
+        MOV     BP, SP
+
+        PUSH    BX
+        PUSH    CX
+        PUSH    SI
+        PUSH    AX
+
+
+        MOV     CX, 4(BP)       ! N
+        MOV     BX, 6(BP)       ! BX
+        MOV     SI, 0
+
+        ! N = 1
+        CMP     CX, 0
+        JE      EPILOGUE
+        MOV     (BX)(SI), 1
+        ADD     SI, 2
+        DEC     CX
+
+        ! N = 2
+        CMP     CX, 0
+        JE      EPILOGUE
+        MOV     (BX)(SI), 1
+        ADD     SI, 2
+        DEC     CX
+
+ITER:
+        CMP     CX, 0
+        JE      EPILOGUE
+
+        MOV     AX, -2(SI)
+        ADD     AX, -4(SI)
+        MOV     (BX)(SI), AX
+        ADD     SI, 2
+        DEC     CX
+        JMP     ITER
+
+
+EPILOGUE:
+        POP     AX
+        POP     SI
+        POP     CX
+        POP     BX
+        POP     BP
+        RET
+
+
 .SECT .DATA
-arr:    .WORD   0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+arr:    .WORD   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 end:    .BYTE   0
 
 .SECT .BSS
